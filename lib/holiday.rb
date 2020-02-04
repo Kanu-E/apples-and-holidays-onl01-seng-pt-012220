21156e1f8fc3d45d4e26538ea6a068ecd502a03d
Chukwuemeka Kanu <chukwuemekakanu@gmail.com>
2020-02-04 00:53:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,12 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  new_array = []
+  holiday.each do |holiday, array|
+    if array.include?("BBQ")
+      new_array << holiday
+    end
+   end
 end
 
 
