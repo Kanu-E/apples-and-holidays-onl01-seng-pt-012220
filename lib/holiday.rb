9ead81adafbc3f670983a8778d63ad64ab3ec44b
Chukwuemeka Kanu <chukwuemekakanu@gmail.com>
2020-02-04 01:16:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,8 +64,8 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, h|
   puts "#{season.to_s.capitalize}:"
      h.each do |holiday,array|
-       holiday.to_s.split("_")
-       str.map {|w| w.capitalize} .join()
+      str = holiday.to_s.split("_")
+       new_str = str.map {|w| w.capitalize} .join()
     end    
 end
 end
